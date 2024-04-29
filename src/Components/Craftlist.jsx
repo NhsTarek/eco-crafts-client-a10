d8a2a15f65d562cb31b0ee07df8b302e1d312bbb
NhsTarek <htarek275@gmail.com>
2024-04-30 02:05:34 +0600

    added craflist component

--- src/Components/Craftlist.jsx
+++ src/Components/Craftlist.jsx
@@ -1,20 +1,35 @@
 
 
 const Craftlist = ({ i }) => {
-    const { itemName, image, subcategoryName, shortDescription, price, rating, userName, userEmail, stockStatus, customization, processingTime } = i;
+    const { _id, itemName, image, price, rating, stockStatus, customization, } = i;
+
+    const handleDelete = _id => {
+       console.log(_id);
+    }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
                 <img src={image} alt="Shoes" className="rounded-xl w-full" />
             </figure>
-            <div className="card-body items-center text-center">
+            <div className="card-body">
                 <h2 className="card-title">{itemName}</h2>
-                <div className="flex gap-20">
-                <p>Price : {price}</p>
-                <p>{rating}</p>
-                </div>
+
+                <ul>
+                    <li className="font-poppins font-medium">Price : {price}</li>
+                    <li className="font-poppins font-medium">Rating: {rating}</li>
+                    <li className="font-poppins font-medium">Customization: {customization}</li>
+                    <li className="font-poppins font-medium">Stock Status: {stockStatus}</li>
+
+                </ul>
+
+
                 <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary">Update</button>
+                    <button
+                     onClick={() => handleDelete(_id)}
+                     className="btn btn-primary">
+                        Delete
+                    </button>
                 </div>
             </div>
         </div>
